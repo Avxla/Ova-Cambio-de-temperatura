2e1b078fe97bf235632c3405410d3cfbf87f0ed6
AndresAvila <avilabarre68@gmail.com>
2026-07-25 08:49:10 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
 console.log("OVA iniciado correctamente.");
+function mostrarEscala(nombre, boton){
+
+    const escalas=document.querySelectorAll(".escala");
+
+    escalas.forEach(e=>{
+
+        e.classList.add("oculto");
+
+    });
+
+    document.getElementById(nombre).classList.remove("oculto");
+
+    const botones=document.querySelectorAll(".botones button");
+
+    botones.forEach(b=>{
+
+        b.classList.remove("activo");
+
+    });
+
+    boton.classList.add("activo");
+
+}
